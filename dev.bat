402562fd4f3415099a1ed28d07dfb38b6a2ce197
Al Gelders <agelders2021@earthlink.net>
2026-05-17 16:57:14 -0600

    missed files

---
+++ dev.bat
@@ -0,0 +1,2 @@
+@echo off
+cmd /k "call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat" && cd /d "C:\Users\AlAnd\Git Backed Projects\c_code\rigexecutive\cpp""
